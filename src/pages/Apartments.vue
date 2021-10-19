e55ff57f238d304722809cc2ac28b5f1d5c7e7ca
Jose David <david_gh86@hotmail.es>
2021-10-19 23:44:44 +0200

    apartments CRUD working

--- src/pages/Apartments.vue
+++ src/pages/Apartments.vue
@@ -8,10 +8,44 @@
       :rows="rows"
       :columns="columns"
       v-model:pagination="pagination"
+      row-key="_id"
       :loading="loading"
       @request="onRequest"
-    ></q-table>
-    
+    >
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td auto-width>
+            <q-btn size="sm" color="primary" round dense @click="expandRow(props)" icon="edit"></q-btn>&nbsp;
+            <q-btn size="sm" color="primary" round dense @click="deleteApartment(props.key)" icon="delete"></q-btn>
+          </q-td>
+          <q-td
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            {{ col.value }}
+          </q-td>
+        </q-tr>
+        <q-tr v-show="props.expand" :props="props">
+          <q-td colspan="100%">
+            <!-- <div class="text-left">This is expand slot for row above: {{ props.key }} {{ props.row.apartmentName }}.</div> -->
+            <div class="text-left">
+              <q-input v-model="props.row.apartmentName"></q-input>
+              <q-input type="number" v-model="props.row.keys" dense autofocus></q-input>
+              <q-btn color="primary" label="OK" @click="confirmUpdate(props)"></q-btn>
+              <q-btn color="primary" label="Cancel" @click="cancelUpdate(props)"></q-btn>
+            </div>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+    <div>Create new apartment</div>
+    <div>
+      <q-input v-model="newApartmentName"></q-input>
+      <q-input type="number" v-model="newApartmentKey" dense autofocus></q-input>
+      <q-btn color="primary" label="OK" @click="confirmCreate()"></q-btn>
+      <q-btn color="primary" label="Cancel" @click="cancelCreate()"></q-btn>
+    </div>
     
   </div>
 </template>
@@ -21,29 +55,36 @@
 import { defineComponent, ref, onMounted, getCurrentInstance, inject } from "vue";
 
 const columns = [
-        {
-          name: 'name',
-          required: true,
-          label: 'Apartment name',
-          align: 'left',
-          field: 'apartmentName',
-        },
-        {
-          name: 'keys',
-          required: true,
-          label: 'Number of keys',
-          align: 'left',
-          field: 'keys',
-        },
-      ]
+  {
+    
+  },
+  {
+    name: 'name',
+    required: true,
+    label: 'Apartment name',
+    align: 'left',
+    field: 'apartmentName',
+  },
+  {
+    name: 'keys',
+    required: true,
+    label: 'Number of keys',
+    align: 'left',
+    field: 'keys',
+  },
+]
 
 
 
 export default defineComponent({
   name: "Apartments",
   setup () {
+    const newApartmentName = ref('')
+    const newApartmentKey = ref('')
+    const editApartmentName = ref('')
+    const editApartmentKeys = ref()
+    const elementExpanded = ref(false)
     const rows = ref([])
-    const filter = ref('')
     const loading = ref(false)
     const pagination = ref({
       page: 1,
@@ -51,6 +92,56 @@ export default defineComponent({
       rowsNumber: 10
     })
     const serviceApi = inject('api')
+
+    function deleteApartment(apartmentId) {
+      alert("remove "+apartmentId)
+      rows.value = rows.value.filter(apt => apt._id !== apartmentId)
+    }
+
+    function expandRow(properties){
+      if (!elementExpanded.value){
+        editApartmentName.value = properties.row.apartmentName
+        editApartmentKeys.value = properties.row.keys
+        properties.expand = true
+        elementExpanded.value = true
+      } else {
+        properties.expand = false
+        elementExpanded.value = false
+      }
+    }
+
+    function updateApartment(id, name, keys){
+      return new Promise((resolve, reject) => {
+        resolve("OK")
+      });
+    }
+
+    function confirmUpdate(properties) {
+      updateApartment(properties.key, properties.row.apartmentName, properties.row.keys).then(() => {
+        alert(properties.key + ", " + properties.row.apartmentName + ", " +properties.row.keys)
+      }).catch((error) => {
+        alert("Error updating")
+      }).finally(() => {
+        properties.expand = false
+        elementExpanded.value = false
+      })
+    }
+
+    function cancelUpdate(properties) {
+      properties.row.apartmentName = editApartmentName.value
+      properties.row.keys = editApartmentKeys.value
+      properties.expand = false
+      elementExpanded.value = false
+    }
+
+    function confirmCreate(){
+       alert("created "+newApartmentName.value+ " "+ newApartmentKey.value)
+    }
+
+    function cancelCreate(){
+      newApartmentName.value = ''
+      newApartmentKey.value=''
+    }
 
     function onRequest (props) {
 
@@ -92,7 +183,17 @@ export default defineComponent({
       columns,
       rows,
 
-      onRequest
+      newApartmentName,
+      newApartmentKey,
+
+      onRequest,
+      deleteApartment,
+      expandRow,
+      confirmUpdate,
+      cancelUpdate,
+
+      confirmCreate,
+      cancelCreate
     }
   }
 });
