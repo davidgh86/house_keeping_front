2ca026d031ee9ef9e08595320996755e8e47088f
david <david_gh86@hotmail.es>
2021-10-25 19:28:54 +0200

    a

--- src/pages/Arrivals.vue
+++ src/pages/Arrivals.vue
@@ -1,128 +1,304 @@
 <template>
   <div class="wrapper">
     <div class="header">HSK Admin</div>
-    <div class="date">XX/XX/XXXX</div>
 
-    <div class="selectionbox">
-      <div v-for="(comboId, index) in combosIds"
-          :key="comboId" class="apt-row" v-bind:style="getStyleRow(index + 1)">
-        <ArrivalCombo :allApartments="allApartments" 
-          @confirmEdition="confirmEdition(comboId, $event)" 
-          @changeItem="updateApartment(comboId, $event)"
-          @delete="removeElement(comboId)"/>
-      </div>
+    <q-table
+      title="Arrivals"
+      :rows="rows"
+      :columns="columns"
+      v-model:pagination="pagination"
+      row-key="apartment._id"
+      :loading="loading"
+      @request="onRequest"
+    >
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td auto-width>
+            <q-btn size="sm" color="primary" round dense @click="expandRow(props)" icon="edit"></q-btn>&nbsp;
+            <q-btn size="sm" color="primary" round dense @click="deleteArrival(props.key)" icon="delete"></q-btn>
+          </q-td>
+          <q-td
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            {{ col.value }}
+          </q-td>
+        </q-tr>
+        <q-tr v-show="props.expand" :props="props">
+          <q-td colspan="100%">
+            <div class="text-left">
+              <q-input v-model="props.row.email" label="Email"></q-input>
+              <q-input v-model="props.row.role" label="Role"></q-input>
+              <q-btn color="primary" label="OK" @click="confirmUpdate(props)"></q-btn>
+              <q-btn color="primary" label="Cancel" @click="cancelUpdate(props)"></q-btn>
+            </div>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+    <div>Create new user</div>
+    <div>
+      <q-input v-model="newUserName" label="Username"></q-input>
+      <q-input v-model="newPassword" label="Password"></q-input>
+      <q-input v-model="newUserEmail" label="Email"></q-input>
+      <q-input v-model="newUserRole" label="Role"></q-input>
+      <q-btn color="primary" label="OK" @click="confirmCreate()"></q-btn>
+      <q-btn color="primary" label="Cancel" @click="cancelCreate()"></q-btn>
     </div>
-    <div class="selectionbox">
-      <div class="plus" @click="addElement">
-        <i class="fas fa-plus"></i>
-      </div>
-      <div
-        class="confirm"
-        href="www.citybreakapartments.com"
-        @click="goToApartments()"
-      >
-        CONFIRM
-      </div>
-      <img
-        src="https://www.citybreakapartments.com/wp-content/uploads/2017/10/CityBreakApartments2.png"
-        alt="City Break Logo"
-      />
-    </div>
+    
   </div>
 </template>
 
 <script>
-import { v4 as uuidv4 } from "uuid";
-import { defineComponent } from "vue";
-import ArrivalCombo from "components/ArrivalCombo.vue";
+
+import { defineComponent, ref, onMounted, inject } from "vue";
+
+const columns = [
+  {
+    
+  },
+  {
+    name: 'apartment',
+    required: true,
+    label: 'Apartment name',
+    align: 'left',
+    field: 'apartment.apartmentName',
+  },
+  {
+    name: 'expectedKeys',
+    required: true,
+    label: 'Key provided',
+    align: 'left',
+    field: 'expectedKeys',
+  },
+  {
+    name: 'returnedKeys',
+    required: true,
+    label: 'Returned keys',
+    align: 'left',
+    field: 'returnedKeys',
+  },
+  {
+    name: 'checkInDate',
+    required: true,
+    label: 'Check in date',
+    align: 'left',
+    field: 'checkInDate',
+  },
+  {
+    name: 'checkInTimeNull',
+    required: true,
+    label: 'Arrival time confirmed',
+    align: 'left',
+    field: 'checkInTimeNull',
+  },
+  {
+    name: 'checkOutDate',
+    required: true,
+    label: 'Check out date',
+    align: 'left',
+    field: 'checkOutDate',
+  },
+  {
+    name: 'checkOutTimeNull',
+    required: true,
+    label: 'Departure time confirmed',
+    align: 'left',
+    field: 'checkOutTimeNull',
+  },
+  {
+    name: 'cleaningStatus',
+    required: true,
+    label: 'Cleaning Status',
+    align: 'left',
+    field: row => {
+      let arrayStatus = row.cleaningStatus
+      if (!arrayStatus || arrayStatus.length==0){
+        return "Not cleaning necessary"
+      } else {
+        return arrayStatus[arrayStatus.length - 1]
+      }
+    },
+  },
+  {
+    name: 'timeCleaned',
+    required: true,
+    label: 'Time cleaned',
+    align: 'left',
+    field: 'timeCleaned',
+  },
+  {
+    name: 'timeCleaned',
+    required: true,
+    label: 'Time cleaned',
+    align: 'left',
+    field: 'timeCleaned',
+  },
+  {
+    name: 'message',
+    required: true,
+    label: 'Message',
+    align: 'left',
+    field: 'message',
+  }
+]
+
+
 
 export default defineComponent({
   name: "Arrivals",
-  components: {
-    ArrivalCombo
-  },
-  setup() {
-    return {
-      addElement: function (value) {
-        let uuid = uuidv4();
-        this.combosIds.push(uuid);
-      },
-      removeElement: function (uuid) {
-        this.combosIds = this.combosIds.filter(function (value){
-          return value !== uuid
-        })
-        delete this.selectedApartments[uuid];
-      },
-      confirmEdition: function(uuid, apartment){
-        this.updateApartment(uuid, apartment)
-        alert("update send to back")
-        // TODO 
-      },
-      updateApartment: function(uuid, apartment){
-        this.selectedApartments[uuid] = apartment
-      },
-      getStyleRow: function (rowNumber) {
-        return { gridRow: rowNumber };
-      },
-      goToApartments: function () {
-        // TODO send alert list to back
-        alert("Send all alerts to back")
-        this.$router.push("/apartments");
-      },
-    };
-  },
+  setup () {
+    const arrivalDate = ref(new Date())
+    const newApartmentId = ref('')
+    const newExpectedKeys = ref('')
+    const newReturnedKeys = ref('')
+    const newCheckInDate = ref('')
+    const newCheckInTimeNull = ref('')
+    const newCheckOutDate = ref('')
+    const newCheckOutTimeNull = ref('')
+    const editUserEmail = ref('')
+    const editUserRole = ref()
+    const elementExpanded = ref(false)
+    const rows = ref([])
+    const loading = ref(false)
+    const pagination = ref({
+      page: 1,
+      rowsPerPage: 10
+    })
+    const serviceApi = inject('api')
 
-  watch: {
-    getVisibilityStyle: function (uuid) {
-        return { visibility: this.visible[uuid] };
-      },
-  },
+    function deleteArrival(username) {
+      serviceApi.removeUser(username).then((response) => {
+        rows.value = rows.value.filter(usr => usr.username !== username)
+      }).catch((error) => {
+        alert("Remove apartment failed")
+      })
+    }
 
-  mounted() {
-  },
-  data: function () {
+    function expandRow(properties){
+      if (!elementExpanded.value){
+        editUserEmail.value = properties.row.email
+        editUserRole.value = properties.row.role
+        properties.expand = true
+        elementExpanded.value = true
+      } else {
+        properties.expand = false
+        elementExpanded.value = false
+      }
+    }
+
+    function updateUser(username, email, role){
+      return serviceApi.modifyUser(username, {
+        username: username,
+        email: email,
+        role: role
+      })
+    }
+
+    function confirmUpdate(properties) {
+      updateUser(properties.key, properties.row.email, properties.row.role).then(() => {
+      }).catch((error) => {
+        alert("Error updating")
+      }).finally(() => {
+        properties.expand = false
+        elementExpanded.value = false
+      })
+    }
+
+    function cancelUpdate(properties) {
+      properties.row.email = editUserEmail.value
+      properties.row.role = editUserRole.value
+      properties.expand = false
+      elementExpanded.value = false
+    }
+
+    function confirmCreate(){
+       serviceApi.createNewUser({
+         username: newUserName.value,
+         password: newPassword.value,
+         email: newUserEmail.value,
+         role: newUserRole.value
+       }).then((response) => {
+         // TODO loading
+          onRequest({
+            pagination: pagination.value
+          })
+       }).catch((error) => {
+         alert("Apartment creation failed")
+       })
+    }
+
+    function cancelCreate(){
+      newUserEmail.value = ''
+      newUserRole.value =''
+      newUserName.value = ''
+      newPassword.value =''
+    }
+
+    function onRequest (props) {
+
+      // const app = getCurrentInstance()
+      // const serviceApi = app.appContext.config.globalProperties.$api
+
+      const { page, rowsPerPage } = props.pagination
+
+      loading.value = true
+
+      let offset = (page - 1) * rowsPerPage
+      let limit = rowsPerPage
+
+      if (limit === 0){
+        limit = pagination.value.rowsNumber
+      }
+
+      serviceApi.getAllUsers(offset, limit).then(response => {
+        pagination.value.page = response.data.page
+        pagination.value.rowsPerPage = rowsPerPage
+        pagination.value.rowsNumber = response.data.totalDocs
+
+        let returnedData = response.data.docs
+        rows.value.splice(0, rows.value.length, ...returnedData)
+        //rows.value = response.data.docs
+
+        loading.value = false
+      }).catch((error) => {
+        alert(error)
+        loading.value = false
+      });
+    }
+
+    onMounted(() => {
+      // get initial data from server (1st page)
+      onRequest({
+        pagination: pagination.value
+      })
+    })
+
     return {
-      allApartments: [
-        { id: 0, name: "name1", keys: 3, status: "clean", keysDelivered: 3 },
-        { id: 1, name: "name2", time: "11:40", keys: 3, status: "on_cleaning" },
-        {
-          id: 2,
-          name: "name3",
-          time: "12:15",
-          status: "dirty",
-          keys: 5,
-          keysDelivered: 3,
-        },
-        { id: 3, name: "name4", keys: 3, status: "clean", keysDelivered: 3 },
-        { id: 4, name: "name5", time: "11:40", keys: 3, status: "on_cleaning" },
-        {
-          id: 5,
-          name: "name6",
-          time: "12:15",
-          keys: 8,
-          status: "dirty",
-          keysDelivered: 3,
-        },
-      ],
-      combosIds: [],
-      visible: {},
-      selectedApartments: {},
-    };
-  },
-  methods: {},
-  computed: {
-    apartmentNames: function () {
-      return this.allApartments.map((appartment) => {
-        return appartment.name;
-      });
-    },
-    apartmentIds: function () {
-      return this.allApartments.map((appartment) => {
-        return appartment.id;
-      });
-    },
-  },
+      loading,
+      pagination,
+      columns,
+      rows,
+
+      newUserEmail,
+      newUserRole,
+      newUserName,
+      newPassword,
+
+      onRequest,
+      deleteArrival,
+      expandRow,
+      confirmUpdate,
+      cancelUpdate,
+
+      confirmCreate,
+      cancelCreate
+    }
+  }
 });
+
+
 </script>
 
 <style scoped>
@@ -146,58 +322,6 @@ export default defineComponent({
   margin-top: 1rem;
   margin-bottom: 3.125rem;
 }
-div.selectionbox {
-  display: grid;
-  grid-template-columns: 1fr 4fr 1fr;
-  grid-auto-rows: minmax(6.25rem, auto);
-  grid-gap: 0.9375rem;
-  text-align: center;
-  border-top: 6.25rem;
-  font-family: "Arial";
-  font-weight: 300;
-}
-.apt-row {
-  grid-column: 2;
-}
-.apt-edit {
-  margin: 0.2rem;
-  grid-row: 2;
-}
-.plus {
-  grid-column: 3;
-  grid-row: 8;
-  color: white;
-  background-color: #025389;
-  border-radius: 50%;
-  text-align: center;
-  font-size: 3.125rem;
-  vertical-align: middle;
-  font-size: 6.875rem;
-  min-height: 9.375rem;
-}
-.confirm {
-  grid-column: 2;
-  grid-row: 9;
-  border-radius: 1.5625rem;
-  display: block;
-  height: 3rem;
-  width: 10rem;
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
-  font-family: "Arial";
-  font-size: 1rem;
-  font-weight: 200;
-  background-color: #025389;
-  color: white;
-}
-img {
-  grid-column: 2;
-  grid-row: 10;
-  display: block;
-  width: 12rem;
-  height: auto;
-  margin-left: auto;
-  margin-right: auto;
-}
+
+
 </style>
---
+++ src/pages/Arrivalsold.vue
@@ -0,0 +1,203 @@
+<template>
+  <div class="wrapper">
+    <div class="header">HSK Admin</div>
+    <div class="date">XX/XX/XXXX</div>
+
+    <div class="selectionbox">
+      <div v-for="(comboId, index) in combosIds"
+          :key="comboId" class="apt-row" v-bind:style="getStyleRow(index + 1)">
+        <ArrivalCombo :allApartments="allApartments" 
+          @confirmEdition="confirmEdition(comboId, $event)" 
+          @changeItem="updateApartment(comboId, $event)"
+          @delete="removeElement(comboId)"/>
+      </div>
+    </div>
+    <div class="selectionbox">
+      <div class="plus" @click="addElement">
+        <i class="fas fa-plus"></i>
+      </div>
+      <div
+        class="confirm"
+        href="www.citybreakapartments.com"
+        @click="goToApartments()"
+      >
+        CONFIRM
+      </div>
+      <img
+        src="https://www.citybreakapartments.com/wp-content/uploads/2017/10/CityBreakApartments2.png"
+        alt="City Break Logo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { v4 as uuidv4 } from "uuid";
+import { defineComponent } from "vue";
+import ArrivalCombo from "components/ArrivalCombo.vue";
+
+export default defineComponent({
+  name: "Arrivals",
+  components: {
+    ArrivalCombo
+  },
+  setup() {
+    return {
+      addElement: function (value) {
+        let uuid = uuidv4();
+        this.combosIds.push(uuid);
+      },
+      removeElement: function (uuid) {
+        this.combosIds = this.combosIds.filter(function (value){
+          return value !== uuid
+        })
+        delete this.selectedApartments[uuid];
+      },
+      confirmEdition: function(uuid, apartment){
+        this.updateApartment(uuid, apartment)
+        alert("update send to back")
+        // TODO 
+      },
+      updateApartment: function(uuid, apartment){
+        this.selectedApartments[uuid] = apartment
+      },
+      getStyleRow: function (rowNumber) {
+        return { gridRow: rowNumber };
+      },
+      goToApartments: function () {
+        // TODO send alert list to back
+        alert("Send all alerts to back")
+        this.$router.push("/apartments");
+      },
+    };
+  },
+
+  watch: {
+    getVisibilityStyle: function (uuid) {
+        return { visibility: this.visible[uuid] };
+      },
+  },
+
+  mounted() {
+  },
+  data: function () {
+    return {
+      allApartments: [
+        { id: 0, name: "name1", keys: 3, status: "clean", keysDelivered: 3 },
+        { id: 1, name: "name2", time: "11:40", keys: 3, status: "on_cleaning" },
+        {
+          id: 2,
+          name: "name3",
+          time: "12:15",
+          status: "dirty",
+          keys: 5,
+          keysDelivered: 3,
+        },
+        { id: 3, name: "name4", keys: 3, status: "clean", keysDelivered: 3 },
+        { id: 4, name: "name5", time: "11:40", keys: 3, status: "on_cleaning" },
+        {
+          id: 5,
+          name: "name6",
+          time: "12:15",
+          keys: 8,
+          status: "dirty",
+          keysDelivered: 3,
+        },
+      ],
+      combosIds: [],
+      visible: {},
+      selectedApartments: {},
+    };
+  },
+  methods: {},
+  computed: {
+    apartmentNames: function () {
+      return this.allApartments.map((appartment) => {
+        return appartment.name;
+      });
+    },
+    apartmentIds: function () {
+      return this.allApartments.map((appartment) => {
+        return appartment.id;
+      });
+    },
+  },
+});
+</script>
+
+<style scoped>
+.header {
+  grid-row: 1;
+  background-image: linear-gradient(to right, #009eda, #025389);
+  color: white;
+  font-family: "Arial";
+  font-style: normal;
+  font-size: 1.1rem;
+  text-align: center;
+  padding: 0.9375rem;
+}
+.date {
+  grid-column: 1;
+  grid-row: 2;
+  font-family: "Arial";
+  font-style: normal;
+  font-size: 1rem;
+  text-align: center;
+  margin-top: 1rem;
+  margin-bottom: 3.125rem;
+}
+div.selectionbox {
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  grid-auto-rows: minmax(6.25rem, auto);
+  grid-gap: 0.9375rem;
+  text-align: center;
+  border-top: 6.25rem;
+  font-family: "Arial";
+  font-weight: 300;
+}
+.apt-row {
+  grid-column: 2;
+}
+.apt-edit {
+  margin: 0.2rem;
+  grid-row: 2;
+}
+.plus {
+  grid-column: 3;
+  grid-row: 8;
+  color: white;
+  background-color: #025389;
+  border-radius: 50%;
+  text-align: center;
+  font-size: 3.125rem;
+  vertical-align: middle;
+  font-size: 6.875rem;
+  min-height: 9.375rem;
+}
+.confirm {
+  grid-column: 2;
+  grid-row: 9;
+  border-radius: 1.5625rem;
+  display: block;
+  height: 3rem;
+  width: 10rem;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  font-family: "Arial";
+  font-size: 1rem;
+  font-weight: 200;
+  background-color: #025389;
+  color: white;
+}
+img {
+  grid-column: 2;
+  grid-row: 10;
+  display: block;
+  width: 12rem;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
